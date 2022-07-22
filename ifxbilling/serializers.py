973d8db2e7723f1408d428f4508a94af9a884cee
portermahoney <portermahoney@gmail.com>
2022-07-22 09:06:06 -0400

    use select_related in BillingRecordSerializer queryset

--- ifxbilling/serializers.py
+++ ifxbilling/serializers.py
@@ -840,7 +840,7 @@ class BillingRecordViewSet(viewsets.ModelViewSet):
         root = self.request.query_params.get('root')
         invoice_prefix = self.request.query_params.get('invoice_prefix')
 
-        queryset = models.BillingRecord.objects.all()
+        queryset = models.BillingRecord.objects.select_related('product_usage', 'account').all()
 
         if year:
             queryset = queryset.filter(year=year)
