5a5b132e7ed54fa17208bcb9774eb2e2be779cf8
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2023-04-06 09:50:20 -0400

    Add just_dollars template tag that doesn't do penny conversion

--- ifxbilling/templatetags/dollars.py
+++ ifxbilling/templatetags/dollars.py
@@ -1,13 +1,32 @@
-from django import template
+# -*- coding: utf-8 -*-
+
+'''
+Template tags, mostly for billing emails
+'''
 import decimal
+from django import template
 
 register = template.Library()
 
 @register.filter(name='dollars')
 def dollars(pennies):
-    # convert pennies to dollars if digit
+    ''' convert pennies to dollars if digit '''
     if not str(pennies).isdigit():
         return pennies
     cent = decimal.Decimal('0.01')
-    dollars = decimal.Decimal(int(pennies)/100).quantize(cent, decimal.ROUND_HALF_UP)
-    return f'${dollars}'
+    val = decimal.Decimal(int(pennies)/100).quantize(cent, decimal.ROUND_HALF_UP)
+    return f'${val}'
+
+
+@register.filter(name='just_dollars')
+def just_dollars(val):
+    '''
+    Only display as dollars without penny conversion
+    '''
+    try:
+        int(val)
+    except ValueError:
+        return val
+    cent = decimal.Decimal('0.01')
+    valstr, signstr = val_sign(decimal.Decimal(val).quantize(cent, decimal.ROUND_HALF_UP))
+    return f'{signstr}${valstr}'
