fce1dde309b70a6e425aecc48ad7348a0f507463
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2024-05-22 12:46:38 -0400

    Added sort by billing record id to get_billing_record_list.  This helps splits from an individual stay together in the listing

--- ifxbilling/views.py
+++ ifxbilling/views.py
@@ -459,7 +459,7 @@ def get_billing_record_list(request):
         sql += ' where '
         sql += ' and '.join(where_clauses)
 
-    sql += ' order by year, month, account_organization, start_date'
+    sql += ' order by year, month, account_organization, start_date, billing_record_id'
 
     try:
 
